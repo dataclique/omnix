5b7143807be0872eab11c8644ec7f4ef7356f801
0xgleb <gleb.dianov@protonmail.com>
2026-06-13 02:10:10 -0300

    chore: bump nixpkgs to nixos-26.05 and refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Composable Nix infrastructure for DigitalOcean deployments";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     ragenix.url = "github:yaxitech/ragenix";
